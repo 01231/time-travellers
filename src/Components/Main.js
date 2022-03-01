627e4e227a3770ad657ad861b097a1a0c3b2fcb2
noahliechti <noah.liechti@gmail.com>
2022-03-01 16:26:35 +0100

    display new components on homepage

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,46 @@
 import React from "react";
-import { Button } from "@mui/material";
+import { Button, Container } from "@mui/material";
+import ThemeToggle from "./ThemeToggle";
+import URLInput from "./URLInput";
+
+const tweetURLPattern =
+  /^((?:http:\/\/)?|(?:https:\/\/)?)?(?:www\.)?twitter\.com\/(\w+)\/status\/(\d+)$/i;
 
 function Main({ account, network, getAccount }) {
+  const [state, setState] = React.useState({
+    theme: "light",
+    // language: "en",
+    tweetURL: "",
+    invalidTweetURLMessage: "",
+    formErrorMessage: "",
+  });
+
+  const handleChange = (target) => {
+    const { value, name } = target;
+
+    if (name === "tweetURL") {
+      const trimmedURL = value.split("?")[0];
+      const invalidTweetURLMessage =
+        value && !tweetURLPattern.test(trimmedURL)
+          ? "This URL is not valid."
+          : "";
+
+      setState({
+        ...state,
+        [name]: trimmedURL,
+        invalidTweetURLMessage: invalidTweetURLMessage,
+      });
+      return;
+    }
+
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
   return (
-    <div>
+    <Container maxWidth="lg">
       <div>{account}</div>
       <div>
         {network.name}: {network.chainId}
@@ -12,7 +49,13 @@ function Main({ account, network, getAccount }) {
       <Button variant="contained" onClick={getAccount}>
         Log in with Metamask
       </Button>
-    </div>
+      <ThemeToggle defaultTheme={state.theme} handleChange={handleChange} />
+      <URLInput
+        state={state}
+        // formIsSubmitting={formIsSubmitting}
+        handleChange={handleChange}
+      />
+    </Container>
   );
 }
 
