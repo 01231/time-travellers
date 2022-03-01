0db0f06bb3e6b213408bafe327b94b1452cc2428
noahliechti <noah.liechti@gmail.com>
2022-03-01 16:25:30 +0100

    add themetoggle component

---
+++ src/Components/ThemeToggle.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { ToggleButtonGroup, ToggleButton } from "@mui/material";
+
+function ThemeToggle({ defaultTheme, handleChange: propsChange }) {
+  const [theme, setTheme] = React.useState(defaultTheme);
+
+  const handleChange = (e, newTheme) => {
+    if (newTheme) {
+      setTheme(newTheme);
+    }
+    propsChange(e.target);
+  };
+  return (
+    <ToggleButtonGroup
+      fullWidth
+      value={theme}
+      exclusive
+      onChange={handleChange}
+      size="large"
+      sx={{ mt: 2 }}
+    >
+      <ToggleButton name="theme" value="light" aria-label="light theme">
+        Light
+      </ToggleButton>
+      <ToggleButton name="theme" value="dark" aria-label="dark theme">
+        Dark
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
+
+export default ThemeToggle;
