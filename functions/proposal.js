3bb624b8735c7c7020221c8c829515b374ba8691
noahliechti <noah.liechti@gmail.com>
2022-03-01 02:35:51 +0100

    make proposal work

--- functions/proposal.js
+++ functions/proposal.js
@@ -8,6 +8,79 @@ const {
   ALCHEMY_API_KEY_RINKEBY,
 } = require("./utils/config");
 
+const formatDate = (date) => {
+  return Math.round(date / 1e3);
+};
+
+const strategies = JSON.stringify([
+  {
+    name: "erc20-balance-of",
+    params: {
+      symbol: "TTT",
+      address: "0x6BbE04E16056bd9fC6f6fB7ACbbb43c389e0A1A6",
+      decimals: 18,
+    },
+  },
+]);
+
+const plugins = JSON.stringify({
+  safeSnap: {
+    safes: [
+      {
+        network: "4",
+        realityAddress: "0xE2f51aa68B9140F9090e40519Ce1bDcDdBC3e4cA",
+        multiSendAddress: "0x8D29bE29923b68abfDD21e541b9374737B49cdAD",
+        hash: "0xf144c9428ba6e01472283c186fed8633cc05184164c223a6cc6591e7ec7ae7bd",
+        txs: [
+          {
+            hash: "0xf9262886d443634b361db0a4be05ec619415308880eaac35555cc8e2e8e5ac23",
+            nonce: 0,
+            mainTransaction: {
+              operation: "0",
+              nonce: "0",
+              token: {
+                name: "Ether",
+                decimals: 18,
+                symbol: "ETH",
+                logoUri:
+                  "https://safe-transaction-assets.gnosis-safe.io/chains/1/currency_logo.png",
+                address: "main",
+              },
+              recipient: "0x24011E9598937bfBFb27FD4D8E9b8FDA42Fa239f",
+              type: "transferFunds",
+              data: "0x",
+              to: "0x24011E9598937bfBFb27FD4D8E9b8FDA42Fa239f",
+              amount: "10000000000000000",
+              value: "10000000000000000",
+            },
+            transactions: [
+              {
+                operation: "0",
+                nonce: 0,
+                token: {
+                  name: "Ether",
+                  decimals: 18,
+                  symbol: "ETH",
+                  logoUri:
+                    "https://safe-transaction-assets.gnosis-safe.io/chains/1/currency_logo.png",
+                  address: "main",
+                },
+                recipient: "0x24011E9598937bfBFb27FD4D8E9b8FDA42Fa239f",
+                type: "transferFunds",
+                data: "0x",
+                to: "0x24011E9598937bfBFb27FD4D8E9b8FDA42Fa239f",
+                amount: "10000000000000000",
+                value: "10000000000000000",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    valid: true,
+  },
+});
+
 const createProposal = async () => {
   const hub = "https://hub.snapshot.org"; // or https://testnet.snapshot.org for testnet
   const client = new snapshot.Client712(hub);
@@ -22,17 +95,17 @@ const createProposal = async () => {
   const blockNumber = (await provider.getBlock()).number;
 
   const receipt = await client.proposal(signer, signer.address, {
-    space: "Time-Travellers-DAO",
-    type: "single-choice",
+    space: "3.spaceshot.eth",
+    type: "quadratic",
     title: "Test proposal using Snapshot.js",
-    body: "",
+    body: "haaaaallllllooooo",
     choices: ["Alice", "Bob", "Carol"],
-    start: 1636984800, // TODO: what's this?
-    end: 1637244000,
+    start: formatDate(Date.now()),
+    end: formatDate(Date.now() + 1000 * 60 * 60), // + 60mins
     snapshot: blockNumber, // TODO: how far back do we want to go?
     network: "4",
-    strategies: JSON.stringify({}),
-    plugins: JSON.stringify({}),
+    strategies: strategies,
+    plugins: plugins,
     metadata: JSON.stringify({}),
   });
 };
