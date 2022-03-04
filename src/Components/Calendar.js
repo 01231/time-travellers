92d817d8ba4fbbc44040e7eae8297ea5f5fc2ec2
Stefan Lehmann <bernd49634@web.de>
2022-03-04 23:05:27 +0100

    added event listener for tweet minted

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -5,18 +5,30 @@ import nftAddress from "../config/contracts/map.json";
 
 function Calendar({ provider }) {
   const [allTweets, setAllTweets] = useState([]);
+
+  const NFTContract = new ethers.Contract(
+    nftAddress[4].TimeTravellersNFT,
+    NFT.abi,
+    provider
+  );
+
   async function fetchAllTweets() {
-    const NFTContract = new ethers.Contract(
-      nftAddress[4].TimeTravellersNFT,
-      NFT.abi,
-      provider
-    );
     /* for gas estimation in cant to optimize our contract - 
     const gasCost = await NFTContract.estimateGas.getAllMintedTokens();
     console.log(gasCost); */
     const result = await NFTContract.getAllMintedTokens();
     setAllTweets(result);
   }
+  useEffect(() => {
+    NFTContract.on("TokenCreated", () => {
+      fetchAllTweets();
+    });
+    return () => {
+      NFTContract.removeListener("TokenCreated", () => {
+        fetchAllTweets();
+      });
+    };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
   // Only runs once, first load
   useEffect(() => {
     fetchAllTweets();
@@ -35,12 +47,12 @@ function Calendar({ provider }) {
   } */
   async function mintToken() {
     const signer = provider.getSigner();
-    const NFTContract = new ethers.Contract(
+    const NFTSignerContract = new ethers.Contract(
       nftAddress[4].TimeTravellersNFT,
       NFT.abi,
       signer
     );
-    await NFTContract.mintTweet(
+    await NFTSignerContract.mintTweet(
       "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
       "https://twitter.com/BinanceUS/status/1499457941510004745"
     );
