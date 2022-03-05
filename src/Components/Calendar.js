d1f0fb88f4e3b72d0e2f57b6bce8b30b27fca48d
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-03-05 20:31:42 +0100

    Update Calendar.js

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -128,11 +128,8 @@ function Calendar({ provider }) {
           <div key={index.tokenID}>
             Proposed by: {index.owner}
             <br />
-
             <div>Tweet of the day: {convertUnix(datesState[i])}</div>
             <img alt="tweets" src={finishedImageURL[i]} />
-
-
           </div>
         ))}
       </div>
