887f70e26e97b8fffb9402a86cb978c0e59ae206
Stefan Lehmann <bernd49634@web.de>
2022-07-14 23:00:10 +0200

    removed unnecessary code inside Token contract

--- contracts/TimeTravellersToken.sol
+++ contracts/TimeTravellersToken.sol
@@ -7,9 +7,8 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 //
 contract TimeTravellersToken is ERC20, ReentrancyGuard {
-    mapping(address => uint256) private userToChange;
-
     constructor() ERC20("Time-Travellers-Token", "TTT") {
+        // multi sig wallet, Gnosis-safe
         _mint(0x1104Eed7b5A9d1338a5b4822dFB0d66fF4AC216D, 8000 * 10**18);
         _mint(0x2b4F0b671c96d4E4Bbe8Ca084a037902C0c8929c, 1000 * 10**18);
         _mint(0x24011E9598937bfBFb27FD4D8E9b8FDA42Fa239f, 1000 * 10**18);
